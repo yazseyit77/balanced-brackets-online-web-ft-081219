e5d7e9e0f517a06eea94ef58ace0527e6715c3b5
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-24 23:47:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function isBalanced(str){
+  while (str.length > 0){
+    let len = str.length;
+
+    str = str.replace('()', '');
+    str = str.replace('{}', '');
+    str = str.replace('[]', '');
+    if(str.length === len){
+      return false;
+    }
+  }
+  return true;
+}
